2648d9dc12e3de7959e4c63184fbaf3a2da1c712
Sam Pham <sam.h.pham@gmail.com>
2017-01-10 15:54:59 +0000

    added logout.php

--- signin.php
+++ signin.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>check xss</title>
-  <head>
-    <h1>Sign in</h1>
-    <form action="confirmSignin.php" method="post">
-        Name: <input type="text" name="name"><br>
-        Password: <input type="password" name="password"><br>
-        <input type="submit">
-    </form>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css"/>
+  <title>Sign-in</title>
+<head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="form-group">
+          <h2 class="">Sign-in</h2>
+        </div>
+        <form action="confirmSignin.php" method="post" autocomplete="off">
+          <input type="text" name="name" class="form-control" placeholder="Name"><br>
+          <input type="password" name="password" class="form-control" placeholder="Password"><br>
+          <input type="submit" class="btn btn-block btn-primary">
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
