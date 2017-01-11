ac3ea8fbeb9d6c8aa0fdef6721df55a8d655bb34
Sam Pham <sam.h.pham@gmail.com>
2017-01-11 10:20:03 +0000

    Updated the UI of the uploading and the system itself

--- fileUpload.php
+++ fileUpload.php
@@ -1,4 +1,7 @@
 <?php
+  session_start();
+  $name = $_SESSION["name"];
+
    if(isset($_FILES['file'])){
        $errors= array();
        $file_name = $_FILES['file']['name'];
@@ -22,7 +25,7 @@
           //  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
           //  $uploadOk = 1;
           //  $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
-           move_uploaded_file($file_tmp,"uploads/".$file_name);
+           move_uploaded_file($file_tmp,"uploads/" . $name . "/" .$file_name);
            echo "Success";
           //  $dir = "/Users/Applications/MAMP/htdocs/uploads";
            // Sort in ascending order
@@ -51,16 +54,30 @@
        }
    }
 ?>
-<?php
-  session_start();
-  $name = $_SESSION["name"];
-?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link href="style.css" rel="stylesheet">
+  <title>Snippets</title>
+  <script>https://cdnjs.cloudflare.com/ajax/libs/bootstrap-filestyle/1.2.1/bootstrap-filestyle.js</script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script>
+    $(document).on('click', '.browse', function(){
+      var file = $(this).parent().parent().parent().find('.file');
+      file.trigger('click');
+    });
+    $(document).on('change', '.file', function(){
+      $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+    });
+  </script>
+  <style>
+    .file {
+      visibility: hidden;
+      position: absolute;
+    }
+  </style>
 </head>
 <body>
   <nav class="navbar navbar-inverse navbar-default navbar-static-top" role="navigation">
@@ -82,46 +99,43 @@
       </div>
     </div>
   </nav>
-<h1>File Uploading</h1>
-
-<h2>Files in Account</h1>
-
-<?php
-  //Use Session name to find folder and display files in folder
-  // $userDir = $_SESSION["name"];
-  // if(empty(scandir($userDir))) {
-  //   mkdir($userDir);
-  // }
-  $dir = "uploads";
-  $files = scandir($dir);
-  echo '<ul>';
-  foreach( $files as $file ){
-    if(($file != ".")&&($file != "..")){
-     echo '<li><a href="http://localhost:8888/uploads/' . $file . '">' . $file . '</a></br>';
-   }
-  }
-  echo '</ul>';
-?>
-
-<h2>Upload file</h2>
-
-<form action="" method="POST" enctype="multipart/form-data">
-    <!--Upload file form.-->
-    <input type="file" name="file" />
-    <input type="submit"/>
-
-    <ul>
-        <li>Sent file: <?php echo $_FILES['file']['name'];  ?>
-        <li>File size: <?php echo $_FILES['file']['size'];  ?>
-        <li>File type: <?php echo $_FILES['file']['type'] ?>
-        <li>File URL: <?php echo '<a href="http://localhost:8888/uploads/' . $_FILES['file']['name'] . '">' . $_FILES['file']['name'] . '</a>' ?>
-    </ul>
-</form>
-
-<!-- <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-<script src = "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> -->
-
-
+  <div class="container">
+    <h1>File Storage</h1>
+    <h2>Upload file</h2>
+    <form action="" method="POST" enctype="multipart/form-data">
+      <div class="form-group">
+        <input type="file" name="file" class="file">
+        <div class="input-group col-xs-12">
+          <input type="text" class="form-control input-lg" disabled placeholder="Upload File">
+          <span class="input-group-btn">
+            <button class="browse btn btn-primary input-lg" type="button"><i class="glyphicon glyphicon-search"></i> Browse</button>
+          </span>
+        </div>
+      </div>
+      <input type="submit" class="btn btn-block btn-primary" ><br>
+      <ul>
+          <li>Sent file: <?php echo $_FILES['file']['name'];  ?>
+          <li>File size: <?php echo $_FILES['file']['size'];  ?>
+          <li>File type: <?php echo $_FILES['file']['type'] ?>
+          <li>File URL: <?php echo '<a href="http://localhost:8888/uploads/' . $name . "/" . $_FILES['file']['name'] . '">' . $_FILES['file']['name'] . '</a>' ?>
+      </ul>
+    </form>
+    <h2>Files in Account</h1>
+      <?php
+      $dir = "uploads/" . $name;
+        if (!file_exists($dir)){
+          mkdir($dir);
+        } else {
+        $files = scandir($dir);
+        echo '<ul>';
+        foreach( $files as $file ){
+          if(($file != ".")&&($file != "..")){
+           echo '<li><a href="http://localhost:8888/uploads/' . $file . '">' . $file . '</a></br>';
+         }
+        }
+        echo '</ul>';
+      }
+      ?>
+  </div>
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -31,199 +31,169 @@
     </div>
   </nav>
   <div class="container">
-  <h1>Profile Page</h1>
-  <?php
-  if ($_GET['status'] == "success"){
-    echo "<div class = 'alert alert-success'>Successfully saved settings!</div>";
-  } else if ($_GET['status'] == "failure" && $_GET['error'] == "incorrectpassword"){
-    echo "<div class = 'alert alert-danger'>Error occurred! Incorrect password was entered. Try again.</div>";
-  } else if ($_GET['status'] == "failure"){
-    echo "<div class = 'alert alert-danger'>Error occurred!" . $_GET['error'] . "</div>";
-  }
-  ?>
-  <h2>Change Username</h1>
-  <form action="changename.php" method="post">
-    <p><label> Current Username:</label><?php echo " $name"; ?></p>
-    <input type="text" class="form-control" name="newName"><br>
-    <input type="submit" class="btn btn-primary">
-  </form>
-  <h2>Change Password</h2>
-
-  <p> Current Password: <?php
-  $servername = "localhost:8889";
-  $username = "root";
-  $password = "root";
-  $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "Connected successfully";
-
-  $sql = "SELECT password FROM users WHERE name='$name'";
-
-  // print_r($conn->query($sql));
-
-  foreach ($conn->query($sql) as $row) {
-        echo $row['password'];
-  }
-  $conn = null;?></p>
-
-  <form action="changepassword.php" method="post">
-    <label>Current Password</label>
-    <input type="password" class="form-control" name="currentPassword"><br>
-    <label>New Password</label>
-    <input type="password" class="form-control" name="newPassword"><br>
-    <input type="submit" class="btn btn-primary">
-  </form>
-
-  <?php
-     $newPassword= $_POST["newPassword"];
-
-     if(!empty($_POST["newPassword"])) {
-       $servername = "localhost:8889";
-       $username = "root";
-       $password = "root";
-
-       // Create connection
-      try {
-        $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "UPDATE users SET password='$newPassword' WHERE name='$came'";
-        $stmt = $conn->prepare($sql);
-        // execute the query
-        $stmt->execute();
-
-        // echo a message to say the UPDATE succeeded
-        echo $stmt->rowCount() . " records UPDATED successfully";
-      }
-      catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-      }
-
-      $conn = null;
+    <h1>Profile Page</h1>
+    <?php
+    if ($_GET['status'] == "success"){
+      echo "<div class = 'alert alert-success'>Successfully saved settings!</div>";
+    } else if ($_GET['status'] == "failure" && $_GET['error'] == "incorrectpassword"){
+      echo "<div class = 'alert alert-danger'>Incorrect password was entered.</div>";
+    } else if ($_GET['status'] == "failure"){
+      echo "<div class = 'alert alert-danger'>Error occurred!" . $_GET['error'] . "</div>";
     }
-  ?>
+    ?>
+    <h2>Change Username</h1>
+    <form action="changename.php" method="post">
+      <p><label> Current Username:</label><?php echo " $name"; ?></p>
+      <input type="text" class="form-control" name="newName"><br>
+      <input type="submit" class="btn btn-primary">
+    </form>
+    <h2>Change Password</h2>
 
+    <p> Current Password: <?php
+    $servername = "localhost:8889";
+    $username = "root";
+    $password = "root";
+    $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
 
-  <h2>Change Icon URL</h2>
+    $sql = "SELECT password FROM users WHERE name='$name'";
 
-  <p> Current Icon URL: <?php
-  $currentname = "Will";
-  $servername = "localhost:8889";
-  $username = "root";
-  $password = "root";
-  $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "Connected successfully";
+    // print_r($conn->query($sql));
 
-  $sql = "SELECT iconURL FROM users WHERE name='$currentName'";
-
-  foreach ($conn->query($sql) as $row) {
-        print $row['iconURL'] . "\t";
+    foreach ($conn->query($sql) as $row) {
+          echo $row['password'];
     }
-  $conn = null;
-  // $name = "Icon URL";
-  // echo $name; ?></p>
+    $conn = null;?></p>
 
-  <form action="" method="post">
-  New Icon URL: <input type="text" name="newURL"><br>
-  <input type="submit">
-  </form>
+    <form action="changepassword.php" method="post">
+      <label>Current Password</label>
+      <input type="password" class="form-control" name="currentPassword"><br>
+      <label>New Password</label>
+      <input type="password" class="form-control" name="newPassword"><br>
+      <input type="submit" class="btn btn-primary">
+    </form>
 
-  <?php
-     $newURL= $_POST["newURL"];
-     $currentname = "Will";
+    <h2>Change Icon URL</h2>
 
-     if(!empty($_POST["newURL"])) {
-       $servername = "localhost:8889";
-       $username = "root";
-       $password = "root";
-
-       // Create connection
-      try {
-        $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-
-        $sql = "UPDATE users SET iconURL='$newURL' WHERE name='$currentname'";
-
-        $stmt = $conn->prepare($sql);
-
-        // execute the query
-        $stmt->execute();
-
-        // echo a message to say the UPDATE succeeded
-        echo $stmt->rowCount() . " records UPDATED successfully";
-      }
-      catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-      }
-
-      $conn = null;
-    }
-  ?>
-
-  <h2>Change Private Snippet</h2>
-
-  <p> Current Private Snippet: <?php
+    <p> Current Icon URL: <?php
     $currentname = "Will";
     $servername = "localhost:8889";
     $username = "root";
     $password = "root";
     $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
+    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT snippet FROM users WHERE name='$currentName'";
+    // echo "Connected successfully";
+
+    $sql = "SELECT iconURL FROM users WHERE name='$currentName'";
+
     foreach ($conn->query($sql) as $row) {
-          print $row['snippet'] . "\t";
+          print $row['iconURL'] . "\t";
       }
-  $conn = null;?></p>
+    $conn = null;
+    // $name = "Icon URL";
+    // echo $name; ?></p>
 
+    <form action="" method="post">
+    New Icon URL: <input type="text" name="newURL"><br>
+    <input type="submit">
+    </form>
 
-  <form action="" method="post">
-  New Snippet: <textarea name='newSnippet' rows='5' style='width:100%'></textarea><br>
-  <input type="submit">
-  </form>
+    <?php
+       $newURL= $_POST["newURL"];
+       $currentname = "Will";
 
+       if(!empty($_POST["newURL"])) {
+         $servername = "localhost:8889";
+         $username = "root";
+         $password = "root";
 
-  <?php
-     $newSnippet= $_POST["newSnippet"];
-     $currentname = "Will";
+         // Create connection
+        try {
+          $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
+          // set the PDO error mode to exception
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "Connected successfully";
 
-     if(!empty($_POST["newSnippet"])) {
-       $servername = "localhost:8889";
-       $username = "root";
-       $password = "root";
+          $sql = "UPDATE users SET iconURL='$newURL' WHERE name='$currentname'";
 
-       // Create connection
-      try {
-        $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+          $stmt = $conn->prepare($sql);
 
-        $sql = "UPDATE users SET snippet='$newSnippet' WHERE name='$currentName'";
+          // execute the query
+          $stmt->execute();
 
-        $stmt = $conn->prepare($sql);
+          // echo a message to say the UPDATE succeeded
+          echo $stmt->rowCount() . " records UPDATED successfully";
+        }
+        catch(PDOException $e) {
+          echo "Connection failed: " . $e->getMessage();
+        }
 
-        // execute the query
-        $stmt->execute();
-
-        // echo a message to say the UPDATE succeeded
-        echo $stmt->rowCount() . " records UPDATED successfully";
+        $conn = null;
       }
-      catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+    ?>
+
+    <h2>Change Private Snippet</h2>
+
+    <p> Current Private Snippet: <?php
+      $currentname = "Will";
+      $servername = "localhost:8889";
+      $username = "root";
+      $password = "root";
+      $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "SELECT snippet FROM users WHERE name='$currentName'";
+      foreach ($conn->query($sql) as $row) {
+            print $row['snippet'] . "\t";
+        }
+    $conn = null;?></p>
+
+
+    <form action="" method="post">
+    New Snippet: <textarea name='newSnippet' rows='5' style='width:100%'></textarea><br>
+    <input type="submit">
+    </form>
+
+
+    <?php
+       $newSnippet= $_POST["newSnippet"];
+       $currentname = "Will";
+
+       if(!empty($_POST["newSnippet"])) {
+         $servername = "localhost:8889";
+         $username = "root";
+         $password = "root";
+
+         // Create connection
+        try {
+          $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
+          // set the PDO error mode to exception
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "Connected successfully";
+
+          $sql = "UPDATE users SET snippet='$newSnippet' WHERE name='$currentName'";
+
+          $stmt = $conn->prepare($sql);
+
+          // execute the query
+          $stmt->execute();
+
+          // echo a message to say the UPDATE succeeded
+          echo $stmt->rowCount() . " records UPDATED successfully";
+        }
+        catch(PDOException $e) {
+          echo "Connection failed: " . $e->getMessage();
+        }
+
+        $conn = null;
       }
+    ?>
 
-      $conn = null;
-    }
-  ?>
+    <!-- <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-  <!-- <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-  <script src = "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> -->
+    <script src = "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> -->
 
   </div>
 </body>
