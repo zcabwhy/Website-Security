9a9b518fe19a24bc3cbe723f3718b02fa70ed1a4
Sam Pham <sam.h.pham@gmail.com>
2017-01-11 00:27:42 +0000

    changed log out process

--- logOut.php
+++ logOut.php
@@ -3,19 +3,9 @@
   $_SESSION = array();
   @session_destroy();
   if(empty($_SESSION["name"])){
-    $errorMessage = "logged out";
+    $errorMessage = "loggedout";
   }else{
     $errorMessage = "error";
   }
- ?>
-
-
- <!DOCTYPE html>
- <html>
-   <head>
-     <meta charset="utf-8">
-     <title>Logged out</title>
-   <head>
-     <?php echo $errorMessage; ?>
-     <a href = "index.php">"Back to home page"</a>
- </html>
+  header("Location:signin.php?status=" . $errorMessage);
+?>
--- signin.php
+++ signin.php
@@ -27,6 +27,11 @@
         <div class="form-group">
           <h2 class="">Sign-in</h2>
         </div>
+        <?php
+        if ($_GET['status'] == "loggedout"){
+          echo "<div class = 'alert alert-success'>Successfully logged out!</div>";
+        }
+        ?>
         <form action="confirmSignin.php" method="post" autocomplete="off">
           <input type="text" name="name" class="form-control" placeholder="Name" required><br>
           <input type="password" name="password" class="form-control" placeholder="Password" required><br>
