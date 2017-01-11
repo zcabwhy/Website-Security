e6d6c4ffd2952c49ef8a3a0bc593fe68a00ca8be
kazuchan237 <zcabkho@ucl.ac.uk>
2017-01-11 10:36:17 +0000

    modified login and createuser.php

--- login.php
+++ login.php
@@ -14,19 +14,24 @@
     header("Location: signin.php?status=badlogin");
   }
 
-
   function checkPass($servername, $dbusername, $dbpassword, $username, $pw){
-    $conn = new PDO("mysql:host=$servername;dbname=blog_app", $dbusername, $dbpassword);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT password FROM users WHERE name = '$username'";
-    $rows = $conn->query($sql);
-    foreach($rows as $row){
-      $cpass = $row["password"];
-      if($pw == $cpass){
-        return TRUE;
+    try{
+      $conn = new PDO("mysql:host=$servername;dbname=blog_app", $dbusername, $dbpassword);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "SELECT password FROM users WHERE name = '$username'";
+      $rows = $conn->query($sql);
+      foreach($rows as $row){
+        $cpass = $row["password"];
+        if($pw == $cpass){
+          $conn = null;
+          return TRUE;
+        }
       }
+      $conn = null;
+      return FALSE;
+    }catch(PDOException $e) {
+      return FALSE;
     }
-    return FALSE;
   }
 ?>
