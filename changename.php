0c92b58de794749f4674bdbbbcaf5f33638ade30
Sam Pham <sam.h.pham@gmail.com>
2017-01-11 01:21:48 +0000

    William? Lol

---
+++ changename.php
@@ -0,0 +1,23 @@
+<?php
+  session_start();
+  $name = $_SESSION["name"];
+  $newName = $_POST["newName"];
+  if(!empty($_POST["newName"])) {
+    $servername = "localhost:8889";
+    $username = "root";
+    $password = "root";
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=blog_app", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "UPDATE users SET name='$newName' WHERE name='$name'";
+      $_SESSION["name"] = $newName;
+      $stmt = $conn->prepare($sql);
+      $stmt->execute();
+      header("Location: profile.php?status=success");
+    }
+    catch(PDOException $e) {
+      header("Location: profile.php?status=failure&error=" . $e->getMessage());
+    }
+    $conn = null;
+  }
+  ?>
