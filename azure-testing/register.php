f3179b84c53693168ed1b0e59038b6da54179c59
Sam Pham <sam.h.pham@gmail.com>
2017-01-13 01:59:37 +0000

    Added footers

--- azure-testing/register.php
+++ azure-testing/register.php
@@ -48,6 +48,12 @@
       </div>
     </div>
   </div>
+  <div class="footer">
+    <a href="https://github.com/zcabwhy/Website-Security">
+      <img style='display: block; margin-left: auto; margin-right: auto;height:30px; width:30px; margin-bottom: 10px;' src="images/github.png"/>
+    </a>
+    Copyright © 2017 Kazuma Hochin, Sam Pham, William Lam, Zi Sim - Team K SW3
+  </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"><\/script>')</script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
