5002d92d74e532cfe60a77355e004b5309e776e2
Sam Pham <sam.h.pham@gmail.com>
2017-01-14 10:14:12 +0000

    MVC Pattern 3.1

---
+++ azure-testing/view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Snippets</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css"/>
+  <link href="style.css" rel="stylesheet">
+<head>
+<body>
---
+++ azure-testing/view/snippets.php
@@ -0,0 +1,52 @@
+<?php
+  include 'dbconnection.php';
+  session_start();
+  $name = htmlspecialchars($_SESSION["name"]);
+?>
+<?php
+  include_once('header.php');
+  include_once('navbar.php');
+?>
+  <div class="container">
+    <div class="container-fluid text-center">
+      <h2>Your Snippets</h2>
+      <h4><a href='/?action=newsnippet'>Add Snippet</a> | <a href='/?action=allsnippets'>View All Snippets </a></h4>
+      <div class="col-md-10 col-md-offset-1">
+            <?php
+            $name = $_SESSION["name"];
+            $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+            if(!$conn){
+                die("Connection failed: ".mysqli_connect_error());
+            }
+            $sql = "SELECT * FROM messages WHERE name = '$name'";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+              echo '<table class="table">
+                <thead>
+                  <tr>
+                    <th style="text-align: left;">Snippet</th>
+                    <th style="text-align: center;">Delete</th>
+                  </tr>
+                </thead>
+                <tbody>';
+              while($row = mysqli_fetch_assoc($result)) {
+                $message = $row['message'];
+                if ($message == NULL){
+                  $message = "<i>You haven't posted a snippet yet!</i>";
+                }
+                echo "<tr><td style='text-align:left;'>" . $row["message"] . "</td><td style='width: 175px;text-align:center;'><a href='deletesnippet.php?a=" . $row["ID"] . "'>[X]</a></td></tr>";
+              }
+              echo "</tbody>
+                </table>";
+            }
+            else {
+              echo "<i>You haven't posted a snippet yet!</i>";
+            }
+
+            mysqli_close($conn);
+            ?>
+      </div>
+    </div>
+  </div>
+  <?php include_once('footer.php');?>
